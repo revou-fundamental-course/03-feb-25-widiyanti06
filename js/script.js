62d522991d4aa2725ff951507a046ec31b2db8e3
widiyanti06 <iwidiyanti54@gmail.com>
2025-02-12 01:36:47 +0700

    membuat function convert

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 // sintak javascript
+
+function convert(){
+    const celcius = document.getElementById("celcius").value;
+    const fahrenheit = (celcius * 1.8) + 32;
+    document.getElementById("fahrenheit").value = fahrenheit;
+    document.getElementById("cara-konversi").value = celcius + "\xB0C" +" = * 1.8 + 32 = " + fahrenheit + "\xB0F";
+}
