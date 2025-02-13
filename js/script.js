7910c8f53ea0e30f2008af32696ec894fc7d04bf
widiyanti06 <iwidiyanti54@gmail.com>
2025-02-13 20:16:11 +0700

    styling dan cooment

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
-// sintak javascript
+let isFahrenheitToCelsius = true;
 
-function convert(){
-    const celcius = document.getElementById("celcius").value;
-    const fahrenheit = (celcius * 1.8) + 32;
-    document.getElementById("fahrenheit").value = fahrenheit;
-    document.getElementById("cara-konversi").value = celcius + "\xB0C" +" = * 1.8 + 32 = " + fahrenheit + "\xB0F";
+// fungsi konversi suhu
+function convertTemperature() {
+    if (isFahrenheitToCelsius) {
+        let fahrenheit = parseFloat(document.getElementById("temperatureInput").value);
+        let celsius = (fahrenheit - 32) * 5 / 9;
+        document.getElementById("result").value = `${celsius.toFixed(2)}°C`;
+        document.getElementById("cara-konversi").value = `(${fahrenheit}°F - 32) * 5/9 = ${celsius.toFixed(2)}°C`;
+    } else {
+        let celsius = parseFloat(document.getElementById("temperatureInput").value);
+        let fahrenheit = (celsius * 9 / 5) + 32;
+        document.getElementById("result").value = `${fahrenheit.toFixed(2)}°C`;
+        document.getElementById("cara-konversi").value = `(${celsius}°C* 9/5) + 32 = ${fahrenheit.toFixed(2)}°F`;
+    }
+}
+
+//fungsi untuk melakukan reset
+function reset() {
+    document.getElementById("temperatureInput").value = "";
+    document.getElementById("result").value = "";
+    document.getElementById("cara-konversi").value = "";
+}
+
+//fungsi untuk mereverse suhu
+function reverseConversion() {
+    isFahrenheitToCelsius = !isFahrenheitToCelsius;
+    document.getElementById("unitLabel").innerText = isFahrenheitToCelsius ? "Fahrenheit:" : "Celsius:";
+    document.getElementById("resultLabel").innerText = isFahrenheitToCelsius ? "Celsius:" : "Fahrenheit:";
+    document.getElementById("temperatureInput").value = "";
+    document.getElementById("result").value = "";
+    document.getElementById("cara-konversi").value = "";
 }
